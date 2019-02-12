1aceb58e577a9795ac2e8fc9f981ddc059a2f7e5
PGalicia <galicia.patrick@gmail.com>
2019-02-11 20:12:12 -0800

    Integrate mysql

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const mysql = require("mysql");
 
 const helloRoutes = require("./api/routes/hello");
 
+app.use(morgan("dev"));
+
 app.use("/hello", helloRoutes);
+
+app.use("/users", (req, res) => {
+    console.log("Fetching all users");
+
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "sakila"
+    })
+
+    const queryString = "SELECT * FROM actor LIMIT 10";
+
+    connection.query(queryString, (err, rows, fields) => {
+        
+        if(err) {
+            console.log("Failed to fetch users" + err);
+        }
+        res.json(rows)
+    })
+
+})
 
 module.exports = app;
