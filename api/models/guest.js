a2a6cfc9d39153411952f3b263b3730c8a7d6651
patrick <patrickgalicia@patricks-MacBook-Pro.local>
2019-03-22 21:56:00 -0700

    Add some validation for guest model

--- api/models/guest.js
+++ api/models/guest.js
@@ -3,7 +3,7 @@ const db = require("./../../utils/connection");
 
 module.exports = db.define('guest', {
     rowid: { type:Sequelize.INTEGER, primaryKey:true, autoIncrement: true },
-    name: { type: Sequelize.STRING }
+    name: { type: Sequelize.STRING, validate: { is: /^[a-zA-Z]$/ }}
 }, {
     timestamps: false,
     freezeTableName: true
